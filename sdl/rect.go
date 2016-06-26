d944614d09b9eb5c55b2e181acc3ee6da81559b0
qeed <qeed.quan@gmail.com>
2016-06-26 09:18:47 -0400

    up

--- sdl/rect.go
+++ sdl/rect.go
@@ -1,5 +1,12 @@
 package sdl
 
+/*
+#include <SDL.h>
+*/
+import "C"
+
+import "unsafe"
+
 type Point struct {
 	X, Y int32
 }
@@ -10,4 +17,26 @@ type Rect struct {
 
 func (p Point) Add(q Point) Point {
 	return Point{p.X + q.X, p.Y + q.Y}
+}
+
+func (p Point) Sub(q Point) Point {
+	return Point{p.X - q.X, p.Y - q.Y}
+}
+
+func (p Point) In(r Rect) bool {
+	return C.SDL_PointInRect((*C.SDL_Point)(unsafe.Pointer(&p)), (*C.SDL_Rect)(unsafe.Pointer(&r))) != 0
+}
+
+func (r Rect) Empty() bool {
+	return C.SDL_RectEmpty((*C.SDL_Rect)(unsafe.Pointer(&r))) != 0
+}
+
+func (r Rect) Equal(p Rect) bool {
+	return C.SDL_RectEquals((*C.SDL_Rect)(unsafe.Pointer(&r)), (*C.SDL_Rect)(unsafe.Pointer(&p))) != 0
+}
+
+func (r Rect) Enclose(p []Point) Rect {
+	var res C.SDL_Rect
+	C.SDL_EnclosePoints((*C.SDL_Point)(unsafe.Pointer(&p[0])), C.int(len(p)), (*C.SDL_Rect)(unsafe.Pointer(&r)), &res)
+	return Rect{int32(res.x), int32(res.y), int32(res.w), int32(res.h)}
 }
