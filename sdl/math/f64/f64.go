22386437226bb8872242adfa3872e72120f06b16
qeed <qeed.quan@gmail.com>
2016-06-25 23:50:04 -0400

    up

--- sdl/math/f64/f64.go
+++ sdl/math/f64/f64.go
@@ -4,12 +4,40 @@ type Vec2 struct {
 	X, Y float64
 }
 
+func (p Vec2) Add(q Vec2) Vec2 {
+	return Vec2{p.X + q.X, p.Y + q.Y}
+}
+
+func (p Vec2) Sub(q Vec2) Vec2 {
+	return Vec2{p.X - q.X, p.Y - q.Y}
+}
+
+func (p Vec2) Dot(q Vec2) float64 {
+	return p.X*q.X + p.Y*q.Y
+}
+
 type Vec3 struct {
 	X, Y, Z float64
 }
 
+func (p Vec3) Add(q Vec3) Vec3 {
+	return Vec3{p.X + q.X, p.Y + q.Y, p.Z + q.Z}
+}
+
+func (p Vec3) Sub(q Vec3) Vec3 {
+	return Vec3{p.X - q.X, p.Y - q.Y, p.Z - q.Z}
+}
+
+func (p Vec3) Dot(q Vec3) float64 {
+	return p.X*q.X + p.Y*q.Y + p.Z*q.Z
+}
+
 type Vec4 struct {
 	X, Y, Z, W float64
+}
+
+func (p Vec4) Dot(q Vec4) float64 {
+	return p.X*q.X + p.Y*q.Y + p.Z*q.Z + p.W*q.W
 }
 
 type Mat4 [4][4]float64
