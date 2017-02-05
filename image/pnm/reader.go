773ac34ea92fc72792469cdb5e982d6b2e61b66d
qeed <qeed.quan@gmail.com>
2017-02-05 11:53:02 -0500

    .

--- image/pnm/reader.go
+++ image/pnm/reader.go
@@ -2,31 +2,193 @@ package pnm
 
 import (
 	"bufio"
+	"encoding/binary"
+	"errors"
+	"fmt"
 	"image"
+	"image/color"
 	"io"
 )
 
+var (
+	ErrFormat = errors.New("pnm: unsupported format")
+)
+
 func Decode(r io.Reader) (image.Image, error) {
-	return nil, nil
+	d := decoder{r: r}
+	err := d.decodeHeader()
+	if err != nil {
+		return nil, err
+	}
+
+	b := image.Rect(0, 0, d.w, d.h)
+	m := image.NewRGBA(b)
+	for y := b.Min.Y; y < b.Max.Y; y++ {
+		for x := b.Min.X; x < b.Max.X; x++ {
+			m.Set(x, y, d.readColor())
+		}
+	}
+
+	return m, d.err
 }
 
 func DecodeConfig(r io.Reader) (image.Config, error) {
-	return image.Config{}, nil
-}
+	d := decoder{r: r}
+	err := d.decodeHeader()
+	if err != nil {
+		return image.Config{}, err
+	}
 
-func decodeHeader(r io.Reader) (header, error) {
-	return header{}, nil
+	return image.Config{
+		ColorModel: color.RGBAModel,
+		Width:      d.w,
+		Height:     d.h,
+	}, nil
 }
 
 type header struct {
-	typ  int
-	w, h int
+	format int
+	maxval int
+	w, h   int
 }
 
 type decoder struct {
-	r io.Reader
-	b *bufio.Reader
+	r    io.Reader
+	b    *bufio.Reader
+	err  error
+	bits uint8
+	bw   uint8
 	header
+}
+
+func (d *decoder) decodeHeader() error {
+	d.b = bufio.NewReader(d.r)
+
+	var sig [2]byte
+	fmt.Fscanf(d.b, "%c%c", &sig[0], &sig[1])
+	switch string(sig[:]) {
+	case "P1", "P2", "P3", "P4", "P5", "P6":
+		d.format = int(sig[1] - '0')
+	default:
+		return ErrFormat
+	}
+
+	d.w = d.readInt()
+	d.h = d.readInt()
+	d.maxval = d.readInt()
+
+	return d.err
+}
+
+func (d *decoder) peek() uint8 {
+	if d.err != nil {
+		return 0
+	}
+
+	p, err := d.b.Peek(1)
+	if err != nil {
+		d.err = err
+		return 0
+	}
+	return p[0]
+}
+
+func (d *decoder) getch() byte {
+	if d.err != nil {
+		return 0
+	}
+	ch, err := d.b.ReadByte()
+	if err != nil {
+		d.err = err
+		return 0
+	}
+	return ch
+}
+
+func (d *decoder) skipws() {
+	for {
+		switch d.peek() {
+		case ' ', '\t', '\n', '\r':
+			d.getch()
+			continue
+		case '#':
+			for {
+				ch := d.getch()
+				if ch == '\n' || ch == 0 {
+					break
+				}
+			}
+		default:
+			return
+		}
+	}
+}
+
+func (d *decoder) readInt() int {
+	if d.err != nil {
+		return 0
+	}
+
+	d.skipws()
+	var n int
+	_, d.err = fmt.Fscan(d.b, &n)
+	return n
+}
+
+func (d *decoder) readColor() color.RGBA {
+	var (
+		c color.RGBA
+		y uint8
+	)
+
+	if d.err != nil {
+		return c
+	}
+
+	d.skipws()
+	switch d.format {
+	case 1, 2:
+		_, d.err = fmt.Fscanf(d.b, "%d", &y)
+		if d.format == 1 && y != 0 {
+			y = 255
+		}
+		c = color.RGBA{y, y, y, 255}
+	case 3:
+		_, d.err = fmt.Fscanf(d.b, "%d %d %d", &c.R, &c.G, &c.B)
+		c.A = 255
+	case 4:
+		if d.bits == 0 {
+			d.err = binary.Read(d.b, binary.LittleEndian, &d.bw)
+			d.bits = 8
+		} else {
+			y := uint8(0)
+			if d.bw&(1<<(7-(d.bits-1))) != 0 {
+				y = 255
+			}
+			c = color.RGBA{y, y, y, 255}
+			d.bits--
+		}
+	case 5:
+		d.err = binary.Read(d.b, binary.LittleEndian, &y)
+		c = color.RGBA{y, y, y, 255}
+	case 6:
+		var p [3]uint8
+		d.err = binary.Read(d.b, binary.LittleEndian, p)
+		c = color.RGBA{p[0], p[1], p[2], 255}
+	}
+
+	s := 255 / float64(d.maxval)
+	cr := float64(c.R) * s
+	cg := float64(c.G) * s
+	cb := float64(c.B) * s
+	return color.RGBA{clamp(cr), clamp(cg), clamp(cb), c.A}
+}
+
+func clamp(x float64) uint8 {
+	if x > 255 {
+		return 255
+	}
+	return uint8(x)
 }
 
 func init() {
--- image/pnm/writer.go
+++ image/pnm/writer.go
@@ -2,6 +2,7 @@ package pnm
 
 import (
 	"bufio"
+	"encoding/binary"
 	"fmt"
 	"image"
 	"image/color"
@@ -19,8 +20,19 @@ func Encode(w io.Writer, m image.Image, o *Options) error {
 
 	b := bufio.NewWriter(w)
 	r := m.Bounds()
+	bits := uint(0)
+	bw := uint8(0)
 
-	fmt.Fprintf(w, "P%d", o.Format)
+	fmt.Fprintf(w, "P%d\n", o.Format)
+	fmt.Fprintf(w, "%d %d\n", r.Dx(), r.Dy())
+	switch o.Format {
+	case 1, 4:
+	case 2, 3, 5, 6:
+		fmt.Fprintf(w, "255\n")
+	default:
+		return ErrFormat
+	}
+
 	for y := r.Min.Y; y < r.Max.Y; y++ {
 		for x := r.Min.X; x < r.Max.X; x++ {
 			p := m.At(x, y)
@@ -31,11 +43,31 @@ func Encode(w io.Writer, m image.Image, o *Options) error {
 					c.Y = 1
 				}
 				fmt.Fprintf(w, "%d ", c.Y)
+
 			case 3:
 				c := color.RGBAModel.Convert(p).(color.RGBA)
 				fmt.Fprintf(w, "%d %d %d", c.R, c.G, c.B)
-			default:
-				return fmt.Errorf("unknown pnm format")
+
+			case 4:
+				c := color.RGBAModel.Convert(p).(color.Gray)
+				if c.Y != 0 {
+					bw |= 1 << (7 - bits)
+				}
+
+				if bits++; bits == 8 {
+					binary.Write(w, binary.LittleEndian, bw)
+					bits = 0
+					bw = 0
+				}
+
+			case 5:
+				c := (color.GrayModel.Convert(p)).(color.Gray)
+				binary.Write(w, binary.LittleEndian, c.Y)
+
+			case 6:
+				c := color.RGBAModel.Convert(p).(color.RGBA)
+				p := [3]uint8{c.R, c.G, c.B}
+				binary.Write(w, binary.LittleEndian, p)
 			}
 
 			if x+1 < r.Max.X {
@@ -47,6 +79,10 @@ func Encode(w io.Writer, m image.Image, o *Options) error {
 		case 1, 2, 3:
 			fmt.Fprintf(w, "\n")
 		}
+	}
+
+	if bits != 0 {
+		binary.Write(w, binary.LittleEndian, bw)
 	}
 
 	return b.Flush()
