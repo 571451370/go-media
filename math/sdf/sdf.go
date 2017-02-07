f647985389f87f63ae342108b42c4a77df734dc3
qeed <qeed.quan@gmail.com>
2017-02-07 01:30:14 -0500

    .

--- math/sdf/sdf.go
+++ math/sdf/sdf.go
@@ -18,32 +18,62 @@ func Box(p, b f64.Vec3) float64 {
 }
 
 func Torus(p f64.Vec3, t f64.Vec2) float64 {
-	l := f64.Vec2{p.X, p.Z}.Len()
-	q := f64.Vec2{l - t.X, p.Y}
+	q := f64.Vec2{p.XZ().Len() - t.X, p.Y}
 	return q.Len() - t.Y
 }
 
-func Cylinder(p f64.Vec3, h f64.Vec2) float64 {
-	l := f64.Vec2{p.X, p.Z}.Len()
-	q := f64.Vec2{l, p.Y}.Abs()
-	d := q.Sub(h)
-	t1 := math.Min(d.MaxComp(), 0)
-	t2 := d.Max(0).Len()
-	return t1 + t2
+func Cylinder(p, c f64.Vec3) float64 {
+	t1 := p.XZ()
+	t2 := c.XY()
+	t3 := t1.Distance(t2)
+	return t3 - c.Z
 }
 
-func Cone(p, c f64.Vec3) float64 {
-	l := f64.Vec2{p.X, p.Z}.Len()
-	q := f64.Vec2{l, p.Y}
-	d1 := -q.Y - c.Z
-	d2 := math.Max(q.Dot(f64.Vec2{c.X, c.Y}), q.Y)
-	t1 := f64.Vec2{d1, d2}.Max(0).Len()
-	t2 := math.Min(math.Max(d1, d2), 0)
-	return t1 + t2
+func Cone(p f64.Vec3, c f64.Vec2) float64 {
+	q := p.XY().Len()
+	return c.Dot(f64.Vec2{q, p.Z})
+}
+
+func HexPrism(p f64.Vec3, h f64.Vec2) float64 {
+	q := p.Abs()
+	return math.Max(q.Z-h.Y, math.Max((q.X*0.866025+q.Y*0.5), q.Y)-h.X)
 }
 
 func Plane(p f64.Vec3, n f64.Vec4) float64 {
-	return p.Dot(n.V3()) + n.W
+	return p.Dot(n.XYZ()) + n.W
+}
+
+func Triangle(p, a, b, c f64.Vec3) float64 {
+	ba := b.Sub(a)
+	pa := p.Sub(a)
+	cb := c.Sub(b)
+	pb := p.Sub(b)
+	ac := a.Sub(c)
+	pc := p.Sub(c)
+	nor := ba.Cross(ac)
+
+	s1 := f64.Sign(ba.Cross(nor).Dot(pa))
+	s2 := f64.Sign(cb.Cross(nor).Dot(pb))
+	s3 := f64.Sign(ac.Cross(nor).Dot(pc))
+	if math.Sqrt(s1+s2+s3) < 2 {
+		t1 := f64.Clamp(ba.Dot(pa)/ba.Dot(ba), 0, 1)
+		x1 := ba.Scale(t1).Sub(pa)
+		p1 := x1.Dot(x1)
+
+		t2 := f64.Clamp(cb.Dot(pb)/cb.Dot(cb), 0, 1)
+		x2 := cb.Scale(t2).Sub(pb)
+		p2 := x2.Dot(x2)
+
+		t3 := f64.Clamp(ac.Dot(pc)/ac.Dot(ac), 0, 1)
+		x3 := ac.Scale(t3).Sub(pc)
+		p3 := x3.Dot(x3)
+
+		return math.Min(p1, math.Min(p2, p3))
+	}
+
+	t1 := nor.Dot(pa)
+	t2 := nor.Dot(nor)
+	return t1 * t1 / t2
 }
 
 func OpS1(d1, d2 float64) float64 {
