5a6d3e43aa68c18fee9460b066bf44af46d87cda
qeed <qeed.quan@gmail.com>
2018-12-01 16:22:00 -0500

    up

---
+++ image/chroma/distance.go
@@ -0,0 +1,32 @@
+package chroma
+
+import (
+	"image/color"
+	"math"
+)
+
+func DistanceLPRGB(a, b color.Color, p float64) float64 {
+	x := color.RGBAModel.Convert(a).(color.RGBA)
+	y := color.RGBAModel.Convert(b).(color.RGBA)
+
+	d1 := math.Pow(float64(x.R-y.R), p)
+	d2 := math.Pow(float64(x.G-y.G), p)
+	d3 := math.Pow(float64(x.B-y.B), p)
+	return math.Pow(d1+d2+d3, 1/p)
+}
+
+func DistanceL2RGB(a, b color.Color) float64 {
+	return DistanceLPRGB(a, b, 2)
+}
+
+// https://www.compuphase.com/cmetric.htm
+func DistanceWL2RGB(a, b color.Color) float64 {
+	x := color.RGBAModel.Convert(a).(color.RGBA)
+	y := color.RGBAModel.Convert(b).(color.RGBA)
+
+	rm := float64(x.R+y.R) / 2
+	cr := float64(x.R - y.R)
+	cg := float64(x.G - y.G)
+	cb := float64(x.B - y.B)
+	return math.Sqrt(((512+rm)*cr*cr)/256 + 4*cg*cg + ((767-rm)*cb*cb)/256)
+}
